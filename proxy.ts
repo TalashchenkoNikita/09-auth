cdc385e6d06bc047bbf16d6fd3008772fb1f507d
acenickace <acenickace53@gmail.com>
2026-08-08 23:47:41 +0300

    final fix

--- proxy.ts
+++ proxy.ts
@@ -48,7 +48,7 @@ export async function proxy(request: NextRequest) {
   }
 
   if (isAuthenticated && isPublicRoute) {
-    return NextResponse.redirect(new URL("/profile", request.url));
+    return NextResponse.redirect(new URL("/", request.url));
   }
 
   return response;
