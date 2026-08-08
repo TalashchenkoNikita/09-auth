212d5f4affcc09c594d1332925dcb55aea38a059
acenickace <acenickace53@gmail.com>
2026-08-08 23:19:39 +0300

    new fix

--- app/(private-routes)/layout.tsx
+++ app/(private-routes)/layout.tsx
@@ -1,14 +1,7 @@
 export default function PrivateRoutesLayout({
   children,
-  modal,
 }: Readonly<{
   children: React.ReactNode;
-  modal: React.ReactNode;
 }>) {
-  return (
-    <>
-      {children}
-      {modal}
-    </>
-  );
+  return <>{children}</>;
 }
